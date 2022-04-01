1efa40550ea4439da0dbd15fe001dde75209c3d3
Nicolas <nicolasabdoul@outlook.fr>
2022-04-02 01:37:04 +0200

    Add script to create post of work

---
+++ initpost.js
@@ -0,0 +1,60 @@
+const { exec } = require('child_process');
+const fs = require("fs-extra");
+const path = require("path");
+
+
+const gitConfigFile = ".gitconfig"
+exec(` FILE=${gitConfigFile} &&  if test -f "$FILE"; then echo "$FILE exists."; fi`, (error, stdout, stderr) => {
+  if (error) {
+    console.log(error.stack);
+    console.log('Error code: ', error.code);
+    console.log('Signal received: ', error.signal);
+  }
+  
+  if (!stdout) {
+    console.log(`${gitConfigFile} file doesn't exist`)
+    console.log(`--- read the template`)
+
+    // read the template
+    let gitConfigTemplate = fs.readFileSync(
+      `${__dirname}/templates/gitconfig.template`,
+      "utf8"
+    ); 
+    gitConfigTemplate = gitConfigTemplate
+    .replace(/{{user}}/gm, process.argv[2])
+    .replace(/{{email}}/gm, process.argv[3]);
+  
+    console.log(`--- write the template`)
+    fs.writeFileSync(
+      `${gitConfigFile}`,
+      gitConfigTemplate,
+      function (err) {
+        if (err) return console.log(err);
+      }
+    );
+    console.log(`${gitConfigFile} created`);
+  } else {
+    console.log(`${gitConfigFile} file exist `)
+  }
+});
+
+exec(`sh initpost.sh`, (error, stdout, stderr) => {
+  if (error) {
+    console.log(error.stack);
+    console.log('Error code: ', error.code);
+    console.log('Signal received: ', error.signal);
+  }
+  
+  console.log(stdout)
+});
+
+
+exec('ls -lah', (error, stdout, stderr) => {
+  if (error) {
+    console.log(error.stack);
+    console.log('Error code: ', error.code);
+    console.log('Signal received: ', error.signal);
+  }
+  
+  console.log("il existe",stdout)
+});)
---
+++ initpost.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+touch boubou.txt
+cd ~ 
+
+echo "Instaltation of zsh"
+sudo apt install zsh
+
+echo "Instaltation of ohmyzsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Initialization of vscode intall process"
+echo "Update the packages index and install the dependencies"
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+
+echo "Import the Microsoft GPG key"
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+echo "Enable the Visual Studio Code repository"
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+echo "Installation of the Visual Studio Code package"
+sudo apt install code
+
+echo "Upgrade the package index"
+sudo apt update
+sudo apt upgrade
